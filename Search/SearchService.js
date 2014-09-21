cdf343f36a61d209a1f8dd69229fcc2bd5788e67
happymap <nafan628@gmail.com>
2014-09-20 17:12:51 -0700

    Added fuzzy search service
    Signed-off-by: happymap <nafan628@gmail.com>

--- Search/SearchService.js
+++ Search/SearchService.js
@@ -1,4 +1,4 @@
-var client = require('..//Util/SearchUtil');
+var client = require('../Util/SearchUtil');
 
 var Module = function () {
 };
@@ -9,9 +9,19 @@ Module.prototype.attachRoutes = function (server) {
 
 Module.prototype.query = function (req, res, next) {
 	if (req.params.term) {
-		var q = req.params.term;
+		var keyword = req.params.term;
+		console.log("####search word####: " + keyword);
 		client.search({
-			q: 'user:' + q
+			index: 'search',
+			type: 'publicdishes',
+			body: {
+				query: {
+					fuzzy_like_this: {
+						like_text: keyword,
+						fields: ["publicdish.brand", "publicdish.name"]
+					}
+				}
+			}
 		}).then(function (body) {
 			res.send(body.hits.hits);
 		}, function (error) {
--- data/InitES.py
+++ data/InitES.py
@@ -1,15 +1,6 @@
 #create mapping
 '''
-curl -XPUT 'http://localhost:9200/search/publicdishes/_mapping' -d '
-{
-	"publicdishes" : {
-		"properties" : {
-			"brand" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"},
-			"name" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"}
-		}
-	}
-}
-'
+curl -XPUT 'http://localhost:9200/search/publicdishes/_mapping' -d '{"publicdishes" : {"properties" : {"brand" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"},"name" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"}}}}'
 '''
 
 '''
@@ -29,6 +20,12 @@ curl -XPUT 'http://localhost:9200/search/privatedishes/_mapping' -d '
 import subprocess
 import pymongo
 
+connection_string = 'mongodb://localhost'
+connection = pymongo.Connection(connection_string, safe=True)
+db = connection.search
+publicdishes = db.publicdishes
+privatedishes = db.privatedishes
+
 def createMapping():
 	publicMapping = '{"publicdishes" : {"properties" : {"brand" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"},"name" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"}}}}'
 	privateMapping = '{"privatedishes" : {"properties" : {"brand" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"},"name" : {"type" : "string", "store": true, "index": "analyzed", "null_value": "na"},"usedId" : {"type" : "integer", "index" : "not_analyzed"}}}}'
@@ -36,4 +33,17 @@ def createMapping():
 	subprocess.call(["curl", "-XPUT", "http://localhost:9200/search/privatedishes/_mapping", "-d", privateMapping])
 
 def createData():
+	samplePubDishes = publicdishes.find()[0 : 10]
+	for publicdish in samplePubDishes:
+		publicstr = '{"publicdish": {"brand":"' +  publicdish['brand'] + '","name":"' + publicdish['name'] + '","calories":' + str(publicdish['calories']) + ', "serving":' + str(publicdish['serving']) +'}}'
+		subprocess.call(["curl", "-XPUT", "http://localhost:9200/search/publicdishes/" + str(publicdish['_id']), "-d", publicstr])
+
+	samplePriDishes = privatedishes.find()[0 : 10]
+	for privatedish in samplePriDishes:
+		privatedish = privatedishes.find_one();
+		privatestr = '{"privatedish": {"brand":"' +  privatedish['brand'] + '","name":"' + privatedish['name'] + '","calories":' + str(privatedish['calories']) + ', "serving":' + str(privatedish['serving']) + ', "userId":' + str(privatedish['userId']) +'}}'
 	
+	subprocess.call(["curl", "-XPUT", "http://localhost:9200/search/privatedishes/" + str(privatedish['_id']), "-d", privatestr])
+
+createData()
+createMapping()
